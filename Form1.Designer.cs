2ab1452a94596ffc0210ebc5e8d150bb2ffaf5b6
a0911 <a0911@LAPTOP-HQSJFKUO>
2024-03-22 16:50:40 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace OXGame_遊戲畫面設計_不用寫遊戲邏輯_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            richTextBox1 = new RichTextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(33, 30);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(125, 120);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(164, 30);
+            button1.Name = "button1";
+            button1.Size = new Size(124, 120);
+            button1.TabIndex = 1;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(294, 30);
+            button2.Name = "button2";
+            button2.Size = new Size(124, 120);
+            button2.TabIndex = 2;
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(33, 154);
+            button3.Name = "button3";
+            button3.Size = new Size(124, 120);
+            button3.TabIndex = 3;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(166, 155);
+            button4.Name = "button4";
+            button4.Size = new Size(124, 120);
+            button4.TabIndex = 4;
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(294, 155);
+            button5.Name = "button5";
+            button5.Size = new Size(124, 120);
+            button5.TabIndex = 5;
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(34, 282);
+            button6.Name = "button6";
+            button6.Size = new Size(124, 120);
+            button6.TabIndex = 6;
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(166, 281);
+            button7.Name = "button7";
+            button7.Size = new Size(124, 120);
+            button7.TabIndex = 7;
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(294, 281);
+            button8.Name = "button8";
+            button8.Size = new Size(124, 120);
+            button8.TabIndex = 8;
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(477, 119);
+            button9.Name = "button9";
+            button9.Size = new Size(124, 120);
+            button9.TabIndex = 9;
+            button9.Text = "x";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(477, 81);
+            label1.Name = "label1";
+            label1.Size = new Size(52, 19);
+            label1.TabIndex = 10;
+            label1.Text = "Player";
+            label1.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(richTextBox1);
+            Name = "Form1";
+            Text = "Tic Tac Toe";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RichTextBox richTextBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Label label1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,25 @@
+namespace OXGame_遊戲畫面設計_不用寫遊戲邏輯_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
